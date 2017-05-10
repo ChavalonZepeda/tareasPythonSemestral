7f757d25c2a447f33f51e2ca92789e03d5afea54
ChavalonZepeda <sazper@gmail.com>
2017-05-10 12:12:31 -0500

    orrecicion a la tarea

--- tarea3/tarea3_Ej4_bucle.py
+++ tarea3/tarea3_Ej4_bucle.py
@@ -4,12 +4,12 @@ Salvador Zepeda Pérez
 
 for a in range(0, 2):
 	print("*")
-	for b in range(0, 2):
-		if a==0:
+	if a==0:
+		for b in range(0, 2):
 			print(">>")
-		for c in range(0, 2):
 			if a+b==0:
-				print("***")
-			for d in range(0, 3):
-				if a+b+c==0:
-					print(">>>>>>>>")
+				for c in range(0, 2):
+					print("***")
+					if a+b+c==0:
+						for d in range(0, 3):
+							print(">>>>>>>>")
--- tarea3/tarea3_Ej7_tipos_dato_elementos.py
+++ tarea3/tarea3_Ej7_tipos_dato_elementos.py
@@ -2,6 +2,6 @@
 Salvador Zepeda Pérez
 '''
 
-datalist = [1452, 11.23, 1 + 2j, "Verdadero", 'w3resource', (0, -1), [5, 12], {"clase": 'V', 'section': 'A'} ]
+datalist = [1452, 11.23, 1 + 2j, True, 'w3resource', (0, -1), [5, 12], {"clase": 'V', 'section': 'A'} ]
 for item in datalist:
 	print("tipo de :", item, " es: ", type(item) )	
